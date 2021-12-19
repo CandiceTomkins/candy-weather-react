d25d983b42578d53c172a79ed7cdb8ac23a56df6
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-12-19 08:39:07 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div class="app">
-      <h1>Candy's Weather App</h1>
-      <footer>
-        This project was coded by Candice Tomkins, and is{" "}
-        <a
-          href="https://github.com/CandiceTomkins/candy-weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="app">
+      <div className="container">
+        <h1>Candy's Weather App</h1>
+        <footer>
+          This project was coded by Candice Tomkins, and is{" "}
+          <a
+            href="https://github.com/CandiceTomkins/candy-weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
