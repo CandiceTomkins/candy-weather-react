36ef5a45ecc783902ca719f53d5fa1e51ee9e513
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-12-20 07:16:45 +0000

    updated button

--- src/Weather.js
+++ src/Weather.js
@@ -54,7 +54,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-info w-100 text-light"
+                className="btn btn-info text-light"
               />
             </div>
           </div>
