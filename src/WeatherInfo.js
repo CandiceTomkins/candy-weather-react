0b144a52f6e37653cc40bde7c30d20412da98b06
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-12-20 07:36:24 +0000

    minor CSS changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,15 +9,21 @@ export default function WeatherInfo(props) {
       <h1>{props.data.city}</h1>
       <ul>
         <li>
-          <FormattedDate date={props.data.date} />{" "}
+          <FormattedDate date={props.data.date} />
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
+          <div className="clearfix">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
 
-          <WeatherTemperature celsius={props.data.temperature} />
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
