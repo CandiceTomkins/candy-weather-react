b902b1704f7d57652ee2d1bb60c8e10078ad1bfa
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-12-29 12:58:54 +0000

    Testing Gradient backgrounds

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,23 +13,27 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
+      <div className="col-6">
+        <ul>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>
+            {" "}
+            Wind:
+            {Math.round(props.data.wind)} km/h
+          </li>
+        </ul>
+      </div>
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={62} />
             </div>
 
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/h</li>
-          </ul>
         </div>
       </div>
     </div>
